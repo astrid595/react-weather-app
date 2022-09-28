86b715f1f6d977747557fc5241762b567c447cd3
astrid595 <84007019+astrid595@users.noreply.github.com>
2022-09-28 20:00:09 +1000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project is created by Astrid VU and is {" "}
-      <a href='https://github.com/astrid595/react-weather-app' target="_blank"> open sourced on Github </a>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project is created by Astrid VU and is {" "}
+        <a href="https://github.com/astrid595/react-weather-app" target="_blank"> open sourced on Github </a>
+        </footer>
+      </div>
     </div>
   );
 }
