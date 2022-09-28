aafac18dc03ad141aced839547a007eced34ae16
astrid595 <84007019+astrid595@users.noreply.github.com>
2022-09-28 18:59:05 +1000

    Added the Github rep URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       Weather app
+      <footer>
+        This project is created by Astrid VU and is {" "}
+      <a href='https://github.com/astrid595/react-weather-app' target="_blank"> open sourced on Github </a>
+      </footer>
     </div>
   );
 }
