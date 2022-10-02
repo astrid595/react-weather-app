a6ec3ec473ef1980db9e165b87886239232e25f4
astrid595 <84007019+astrid595@users.noreply.github.com>
2022-10-02 19:03:04 +1100

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather app
+        
+        <Weather />
         <footer>
           This project is created by Astrid VU and is {" "}
         <a href="https://github.com/astrid595/react-weather-app" target="_blank"> open sourced on Github </a>
